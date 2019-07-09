6f20a7b7bbbc538a3bd900f0863ba01256b84e76
Neil Blake <neil0blake@gmail.com>
2019-07-09 21:48:05 +1200

    LaunchX MVP with search shortcuts for: - StackOverflow - GitHub - Google - YouTube - DuckDuckGo - Bing

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # LaunchX
+
+LaunchX provides a VSCode shortcut to launch searches including:
+- StackOverflow
+- GitHub
+- Google
+- YouTube
+- DuckDuckGo
+- Bing
+
+## Example Usage
+
+1. Launch the **Command Palette** `(⇧⌘P)` and type 'Google' to find the **LaunchX: Google search** command.
+2. Hit enter and enter your query, hit enter again to launch inputted query.
+
+Repeat with any other supported search sources. 
+
+## Future Work
+- Configurable sources
+- Queryless launches 
---
+++ src/extension.ts
@@ -0,0 +1,31 @@
+import * as vscode from 'vscode';
+import * as launches from "./launches.json";
+
+export function activate(context: vscode.ExtensionContext) {
+	console.log('LaunchX activated');
+	launches.forEach(({ extension, path }) => {
+		const disposable = registerLaunch(extension, path);
+		context.subscriptions.push(disposable);
+	});
+}
+
+const resolveLaunchXExtension = (extension: string) => `extension.launchx.${extension}`;
+
+const registerLaunch = (extension: string, path: string) => {
+	const disposable = vscode.commands.registerCommand(resolveLaunchXExtension(extension), async () => {
+		const search = await vscode.window.showInputBox({
+			placeHolder: 'Search',
+		});
+		launchSearch(path, search || "");
+	});
+	return disposable;
+};
+
+const resolveSearchUri = (path: string, search: string) => `${path}${encodeURI(search)}`;
+
+const launchSearch = (path: string, search: string) => {
+	const searchUri = resolveSearchUri(path, search);
+	vscode.commands.executeCommand("vscode.open", vscode.Uri.parse(searchUri));
+};
+
+export function deactivate() { }
